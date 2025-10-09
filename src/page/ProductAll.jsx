2a658025d53a5f8a80d0f9f04188616c3b4c7e42
networkSorcerer <gigattfyhig@gmail.com>
2025-10-09 17:18:36 +0900

    react

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -8,23 +8,11 @@ const ProductAll = () => {
   const [query, setQuery] = useSearchParams();
   const getProducts = async () => {
     let searchQuery = query.get("q") || "";
-    let url = `https://my-json-server.typicode.com/networkSorcerer/COTEACH_REACT/products?q=${searchQuery}`; // 기본 URL
-
-    if (searchQuery) {
-      // 상품 이름(name) 필드를 기준으로 검색한다고 가정
-      // 'name_like'는 SQL의 'LIKE'와 비슷한 역할을 합니다.
-      url += `?title_like=${searchQuery}`;
-    }
-
-    // 만약 검색할 필드가 'title'이라면:
-    // url += `?title_like=${searchQuery}`;
-
-    // 만약 전체 텍스트 검색을 고수한다면: (현재 코드와 동일)
-    // url += `?q=${searchQuery}`;
-
-    console.log("요청 URL:", url);
+    console.log("쿼리값은? :", searchQuery);
+    let url = `https://my-json-server.typicode.com/networkSorcerer/COTEACH_REACT_HNM/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
+    console.log("dddd", data);
     setProductList(data);
   };
   useEffect(() => {
